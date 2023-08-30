7c10890ba59ec8ba8df65896b3f14f6013a6308c
Stephan Niendorf <stephan@stephanmitph.com>
2023-08-30 22:16:47 +0200

    add aria label

--- src/components/ContactLinks/index.js
+++ src/components/ContactLinks/index.js
@@ -5,13 +5,19 @@ import Link from "next/link";
 export default function ContactLinks() {
   return (
     <StyledSpan>
-      <Link href="https://github.com/Stephan-mit-Ph">
+      <Link
+        href="https://github.com/Stephan-mit-Ph"
+        aria-label="Link to Github Profile"
+      >
         <Github />
       </Link>
-      <Link href="mailto:stephan@stephanmitph.com">
+      <Link href="mailto:stephan@stephanmitph.com" aria-label="Mail to Stephan">
         <Mail />
       </Link>
-      <Link href="https://www.linkedin.com/in/stephan-niendorf-38538322b/">
+      <Link
+        href="https://www.linkedin.com/in/stephan-niendorf-38538322b/"
+        aria-label="Link to LinkedIn Profile"
+      >
         <Linkedin />
       </Link>
     </StyledSpan>
